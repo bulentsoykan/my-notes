6be9a210cc52a491fdeac4f7f39ccee994beeef5
Bulent Soykan <soykanb@gmail.com>
2023-11-19 19:25:23 +0000

    Nov 19, 2023, 2:25 PM

--- networkgraph.py
+++ networkgraph.py
@@ -6,39 +6,31 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
-# Example: Creating two layers of graphs
-G1 = nx.Graph()
-G1.add_edges_from([(1, 2), (2, 3)])  # Layer 1 edges
-
-G2 = nx.Graph()
-G2.add_edges_from([(4, 5), (5, 6)])  # Layer 2 edges
-
-# Define positions for each node in 3D space
-positions = {
-    1: (0, 1, 1),
-    2: (1, 1, 1),
-    3: (2, 1, 1),
-    4: (0, 0, 0),
-    5: (1, 0, 0),
-    6: (2, 0, 0),
+# Define layers (Graphs)
+layers = {
+    "Layer1": nx.gnp_random_graph(10, 0.4),
+    "Layer2": nx.gnp_random_graph(10, 0.4),
 }
 
-# Draw the nodes
-for node, pos in positions.items():
-    ax.scatter(*pos, s=100)  # Adjust size as needed
+# Define Z positions for each layer
+layer_z_positions = {"Layer1": 0, "Layer2": 1}  # Adjust the spacing as necessary
 
-# Draw the edges
-for edge in G1.edges:
-    xs, ys, zs = zip(*[positions[v] for v in edge])
-    ax.plot(xs, ys, zs, color="blue")
+# Plot each layer
+for layer_name, graph in layers.items():
+    z_pos = layer_z_positions[layer_name]
+    for edge in graph.edges():
+        x, y = zip(*[graph.nodes[node]["pos"] for node in edge])
+        z = [z_pos, z_pos]
+        ax.plot(x, y, z, color="blue")
 
-for edge in G2.edges:
-    xs, ys, zs = zip(*[positions[v] for v in edge])
-    ax.plot(xs, ys, zs, color="red")
+    for node in graph.nodes():
+        x, y = graph.nodes[node]["pos"]
+        ax.scatter(x, y, z_pos, color="red")
 
-# Customize the plot
+# Set labels and title
 ax.set_xlabel("X Axis")
 ax.set_ylabel("Y Axis")
-ax.set_zlabel("Layer (Z Axis)")
+ax.set_zlabel("Z Axis (Layers)")
+plt.title("3D Visualization of Multi-Layer Hypergraphs")
 
 plt.show()
